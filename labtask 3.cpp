e6520b10e53dbfa25eaccbb4d161478efba14482
anikabushrapriyanka <anikabushrapriyanka@gmail.com>
2024-11-16 00:08:38 +0600

    Add files via upload

---
+++ labtask 3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int num ,i;
+    cout<<"Input to find it's Multiplication Table : ";
+    cin>>num;
+
+    for(i=1; i<=10; i++)
+
+    {
+
+        cout<<num<< "x"<<i<<"="<<num*i<<endl;
+    }
+
+
+
+
+
+
+
+
+}
