2dda9af40f406a2b655b99414ea977a6d1e1b3a4
anikabushrapriyanka <anikabushrapriyanka@gmail.com>
2024-11-17 00:09:30 +0600

    Update labtask 3,3.cpp

--- labtask 3,3.cpp
+++ labtask 3,3.cpp
@@ -4,13 +4,23 @@
 using namespace std;
 int main()
 {
+    int firstNum, secondNum, gcd;
+    cout<<"Input the first number: ";
+    cin>>firstNum;
+    cout<<"Input the second number: ";
+    cin>>secondNum;
+    if(firstNum<secondNum){
+            gcd=firstNum;
+    } else if (secondNum<firstNum) {
+            gcd=secondNum;
+    }
+    while(gcd>1){
+        if(firstNum%gcd==0 && secondNum%gcd==0){
+            break;
+        }
+        gcd--;
 
-
-
-
-
-
-
-
+    }
+    cout<<"The Greatest Common Divisor is: "<<gcd;
 
 }
